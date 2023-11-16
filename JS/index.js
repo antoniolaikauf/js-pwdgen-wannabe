1b7b496d1bb67158f5fcfaa99782e818094f3307
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-11-16 14:06:35 +0100

    progetto finito

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+// chiedere dati all'utente
+let nome =prompt("scrivi il tuo nome");
+
+let cognome =prompt("scrivi il tuo cognome")
+
+let colore = prompt("scrivi il tuo coloro preferito");
+// variabile da aggiungere alla password 
+let numero = 23;
+// completamento dalla password e inserimento nella pagina html
+let password = nome + cognome + colore + numero;
+
+document.getElementById("testo").innerHTML=password
